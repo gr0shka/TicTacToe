dcda2916375b29c49aad3bfff72fce24bc963a93
grush <girois19@gmail.com>
2026-07-31 00:00:45 +0500

    Implemented func IsGameOver

--- internal/service/gameServiceImpl.go
+++ internal/service/gameServiceImpl.go
@@ -2,21 +2,103 @@ package service
 
 import "TicTacToe/internal/domain"
 
-type gameService struct {
-	domain.CurrentGame
-}
+type gameService struct{}
 
-func (g gameService) NextTurn(cg domain.CurrentGame) (i, j int, err error) {
+func (g gameService) NextTurn(cg domain.CurrentGame) (int, int, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (g gameService) ValidateGameBoard(gb1, gb2 domain.CurrentGame) (err error) {
+func (g gameService) ValidateNextTurn(cg1, cg2 domain.CurrentGame) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (g gameService) IsGameOver(cg domain.CurrentGame) (w Winner, isEnd bool) {
-	//TODO implement me
-	panic("implement me")
+func (g gameService) IsGameOver(cg domain.CurrentGame) (Winner, bool) {
+	if w := checkHorizontalLines(cg); w != Winner(Draw) {
+		return w, true
+	}
+
+	if w := checkVerticalLines(cg); w != Winner(Draw) {
+		return w, true
+	}
+
+	if w := checkDiagonalLines(cg); w != Winner(Draw) {
+		return w, true
+	}
+
+	isEnd := true
+	for i := 0; i < domain.BoardSize; i++ {
+		for j := 0; j < domain.BoardSize; j++ {
+			if v, _ := cg.Get(i, j); v == domain.FreeCell {
+				isEnd = false
+			}
+		}
+	}
+
+	return Draw, isEnd
+}
+
+func checkHorizontalLines(cg domain.CurrentGame) Winner {
+	w := Winner(Draw)
+
+	for i := 0; i < domain.BoardSize; i++ {
+		firstCell, _ := cg.Get(i, 0)
+		w = Winner(firstCell)
+
+		for j := 0; j < domain.BoardSize; j++ {
+			if cell, _ := cg.Get(i, j); cell != firstCell {
+				w = Winner(Draw)
+				break
+			}
+		}
+	}
+
+	return w
+}
+
+func checkVerticalLines(cg domain.CurrentGame) Winner {
+	w := Winner(Draw)
+
+	for j := 0; j < domain.BoardSize; j++ {
+		firstCell, _ := cg.Get(0, j)
+		w = Winner(firstCell)
+
+		for i := 0; i < domain.BoardSize; i++ {
+			if cell, _ := cg.Get(i, j); cell != firstCell {
+				w = Winner(Draw)
+				break
+			}
+		}
+	}
+
+	return w
+}
+
+func checkDiagonalLines(cg domain.CurrentGame) Winner {
+	firstCell, _ := cg.Get(0, 0)
+	w := Winner(firstCell)
+
+	for i := 0; i < domain.BoardSize; i++ {
+		if cell, _ := cg.Get(i, i); cell != firstCell {
+			w = Winner(Draw)
+			break
+		}
+	}
+
+	if w != Winner(Draw) {
+		return w
+	}
+
+	firstCell, _ = cg.Get(domain.BoardSize-1, domain.BoardSize-1)
+	w = Winner(firstCell)
+
+	for i := domain.BoardSize - 1; i >= 0; i-- {
+		if cell, _ := cg.Get(i, i); cell != firstCell {
+			w = Winner(Draw)
+			break
+		}
+	}
+
+	return w
 }
