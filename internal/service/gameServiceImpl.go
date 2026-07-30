3ad8ccb57a42eef6e5dd1984ead3313c19340bf8
grush <girois19@gmail.com>
2026-07-31 00:18:47 +0500

    implemeted func ValidateNextTurn

--- internal/service/gameServiceImpl.go
+++ internal/service/gameServiceImpl.go
@@ -1,6 +1,10 @@
 package service
 
-import "TicTacToe/internal/domain"
+import (
+	"TicTacToe/internal/domain"
+	"errors"
+	"math"
+)
 
 type gameService struct{}
 
@@ -9,9 +13,46 @@ func (g gameService) NextTurn(cg domain.CurrentGame) (int, int, error) {
 	panic("implement me")
 }
 
-func (g gameService) ValidateNextTurn(cg1, cg2 domain.CurrentGame) error {
-	//TODO implement me
-	panic("implement me")
+func (g gameService) ValidateNextTurn(oldCg, nextCg domain.CurrentGame) error {
+	countChangedCells := 0
+	countFirstPlayerCells := 0
+	countSecondPlayerCells := 0
+
+	for i := 0; i < domain.BoardSize; i++ {
+		for j := 0; j < domain.BoardSize; j++ {
+
+			oldCell, _ := oldCg.Get(i, j)
+			newCell, _ := nextCg.Get(i, j)
+
+			if oldCell != newCell {
+				countChangedCells++
+			}
+
+			if newCell == domain.FirstPlayerOccupied {
+				countFirstPlayerCells++
+			} else {
+				countSecondPlayerCells++
+			}
+		}
+	}
+
+	if countChangedCells > 1 {
+		return errors.New("more than 1 cell modified")
+	}
+	if countChangedCells < 1 {
+		return errors.New("no changes in board")
+	}
+
+	if math.Abs(float64(countFirstPlayerCells)-float64(countSecondPlayerCells)) > 1 {
+
+		if countFirstPlayerCells > countSecondPlayerCells {
+			return errors.New("it should be the second player's turn now")
+		} else {
+			return errors.New("it should be the first player's turn now")
+		}
+	}
+
+	return nil
 }
 
 func (g gameService) IsGameOver(cg domain.CurrentGame) (Winner, bool) {
