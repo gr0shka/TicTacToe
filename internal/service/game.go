2d1c0547838a5c7bee47f9420da23eb20336afad
grush <girois19@gmail.com>
2026-07-30 21:33:35 +0500

    add enum and change declaration

--- internal/service/game.go
+++ internal/service/game.go
@@ -1,9 +1,19 @@
 package service
 
-import "TicTacToe/internal/domain"
+import (
+	"TicTacToe/internal/domain"
+)
+
+type Winner int
+
+const (
+	Draw Winner = iota
+	First
+	Second
+)
 
 type GameService interface {
-	NextTurn() domain.GameBoard
-	ValidationGameBoard() bool
-	IsGameOver() bool
+	NextTurn(cg domain.CurrentGame) (i, j int, err error)
+	ValidateGameBoard(gb1, gb2 domain.CurrentGame) (err error)
+	IsGameOver(cg domain.CurrentGame) (w Winner, isEnd bool)
 }
